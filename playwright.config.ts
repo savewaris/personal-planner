98c4b79bd46aac6b4be6700f10889f96df8e8d05
savewaris <savewaris@gmail.com>
2026-08-29 18:35:50 +0700

    ci: specify npm run test:unit in CI workflow and auto-build in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'npm run start',
+    command: process.env.CI ? 'npm run build && npm run start' : 'npm run dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
